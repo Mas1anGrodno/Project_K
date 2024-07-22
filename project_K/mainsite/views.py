139bb70a24880bcb062f040a5f193b38e60ee987
Kryhin Maksim <masian.grodno@gmail.com>
2024-07-22 19:58:57 +0300

    render request main.html is working

--- project_K/mainsite/views.py
+++ project_K/mainsite/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    html = "<html><body><img src='https://upyachka.io/img/kot/77.gif' alt=\"UPYACHKA\"></body></html>"
-    return HttpResponse(html)
 
 def main(request):
-    html = "main.html"
-    return HttpResponse(html)
+    return render(request, 'main.html')
--- project_K/project_K/urls.py
+++ project_K/project_K/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from mainsite.views import main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("mainsite/", include("mainsite.urls")),
+    path('mainsite/', main),
+    
 ]
 
