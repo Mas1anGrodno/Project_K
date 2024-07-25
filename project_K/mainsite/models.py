7fecb2589d6e5403592f73e930bd960c6d5992c9
Kryhin Maksim <masian.grodno@gmail.com>
2024-07-25 10:42:56 +0300

    add models for hw 26

--- project_K/mainsite/models.py
+++ project_K/mainsite/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# Модель Project должна представлять собой проект с полями названия, описания и даты создания.
+class Project(models.Model):
+    proj_name = models.CharField(max_length=255)
+    proj_desc = models.TextField(blank=True)
+    proj_create = models.DateTimeField(auto_now_add=True)
+
+# Модель Task должна представлять собой задачу, принадлежащую определённому проекту, с полями названия, описания, статуса и приоритета.
+class Task(models.Model):
+    task_name = models.CharField(max_length=255)
+    task_desc = models.TextField(blank=True)
+    task_proir = models.CharField(max_length=255)
