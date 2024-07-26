eb6ed1304f4bd64f7c29a429ca6082d4edea28f3
Kryhin Maksim <masian.grodno@gmail.com>
2024-07-26 15:15:13 +0300

    added sorting ant normal names to admin panel

--- project_K/mainsite/admin.py
+++ project_K/mainsite/admin.py
@@ -3,6 +3,12 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("proj_name", "proj_create")
 
-admin.site.register(Tasks)
+admin.site.register(Project, ProjectAdmin)
+
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ("task_name", "status", "priority")
+
+admin.site.register(Tasks,TasksAdmin)
--- project_K/mainsite/models.py
+++ project_K/mainsite/models.py
@@ -8,6 +8,10 @@ class Project(models.Model):
     proj_desc = models.TextField(blank=True)
     proj_create = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = "Проект"
+        verbose_name_plural = "Проект"
+        ordering = ["proj_create"]
 # Модель Task должна представлять собой задачу, принадлежащую определённому проекту, с полями названия, описания, статуса и приоритета.
 class Tasks(models.Model):
     
@@ -28,11 +32,11 @@ class Tasks(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='medium')
 
+    class Meta:
+        verbose_name = "Задачи"
+        verbose_name_plural = "Задачи"
+        ordering = ["priority"]
+
 # Don't forget 
 # python manage.py makemigrations
 # python manage.py migrate
-
-#class Meta:
-#      verbose_name = 'Project'
-#      verbose_name_plural = 'Project'
-#      ordering = ['cat_create']
