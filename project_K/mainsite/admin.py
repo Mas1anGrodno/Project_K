9de49828f021decb9cdd57889be2d6c5575aebf3
Kryhin Maksim <masian.grodno@gmail.com>
2024-07-26 15:20:49 +0300

    make mirgations

--- project_K/mainsite/admin.py
+++ project_K/mainsite/admin.py
@@ -4,11 +4,11 @@ from .models import *
 # Register your models here.
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ("proj_name", "proj_create")
+    list_display = ("id","proj_name", "proj_create")
 
 admin.site.register(Project, ProjectAdmin)
 
 class TasksAdmin(admin.ModelAdmin):
-    list_display = ("task_name", "status", "priority")
+    list_display = ("id","task_name", "status", "priority")
 
 admin.site.register(Tasks,TasksAdmin)
