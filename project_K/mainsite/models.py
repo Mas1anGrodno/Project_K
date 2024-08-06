2d6f4f5b22bfd4d59ebd9d40e4ef2144ccb1aab8
Kryhin Maksim <masian.grodno@gmail.com>
2024-08-06 13:41:17 +0300

    add padding to base + removing coment

--- project_K/mainsite/models.py
+++ project_K/mainsite/models.py
@@ -13,10 +13,10 @@ class Users(models.Model):
         ordering = ["-date_joined"]
     def __str__(self):
       return self.username
-# Модель Project должна представлять собой проект с полями названия, описания и даты создания.
+    
 class Project(models.Model):
     proj_name = models.CharField(max_length=255, unique=True, verbose_name="Название проекта")
-    proj_desc = models.TextField(blank=True, verbose_name="Описание задачи")
+    proj_desc = models.TextField(blank=True, verbose_name="Описание проекта")
     proj_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания проекта")
 
     class Meta:
@@ -26,7 +26,7 @@ class Project(models.Model):
 
     def __str__(self):
       return self.proj_name
-# Модель Task должна представлять собой задачу, принадлежащую определённому проекту, с полями названия, описания, статуса и приоритета.
+
 class Tasks(models.Model):
     
     STATUS_CHOICES = [
@@ -43,7 +43,7 @@ class Tasks(models.Model):
     task_proj = models.ForeignKey(Project, to_field='proj_name', on_delete=models.CASCADE, related_name='tasks', verbose_name="Проект")
     implementer = models.ForeignKey(Users, to_field='username',null = True ,on_delete=models.CASCADE, related_name='user', verbose_name="Пользователь" )
     task_name = models.CharField(max_length=255, verbose_name="Задача")
-    task_desc = models.TextField(blank=True, verbose_name="Описание проекта")
+    task_desc = models.TextField(blank=True, verbose_name="Описание задачи")
     task_end = models.DateField(null = True, verbose_name="Выполнить до")
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending', verbose_name="Статус задачи")
     priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='medium', verbose_name="Приоритет задачи")
