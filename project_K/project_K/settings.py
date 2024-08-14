483d8c154878ac18d493677c597c0fb461811e38
Kryhin Maksim <masian.grodno@gmail.com>
2024-08-14 13:09:32 +0300

    adding superuser creation if not exist

--- project_K/project_K/settings.py
+++ project_K/project_K/settings.py
@@ -152,3 +152,15 @@ DATABASES = {
         'PORT': env('POSTGRES_PORT'),
     }
 }
+
+
+if os.environ.get('DJANGO_SUPERUSER_USERNAME'):
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+    if not User.objects.filter(username=os.environ['DJANGO_SUPERUSER_USERNAME']).exists():
+        User.objects.create_superuser(
+            username=os.environ['DJANGO_SUPERUSER_USERNAME'],
+            email=os.environ['DJANGO_SUPERUSER_EMAIL'],
+            password=os.environ['DJANGO_SUPERUSER_PASSWORD']
+        )
