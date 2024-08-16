323f1cd05511b1fa5f8e14f53786fd4942523471
Kryhin Maksim <masian.grodno@gmail.com>
2024-08-16 15:54:40 +0300

    add update method

--- project_K/mainsite/serializers.py
+++ project_K/mainsite/serializers.py
@@ -18,7 +18,7 @@ class TaskSerializer(serializers.ModelSerializer):
     task_proj = serializers.CharField(source='task_proj.proj_name')
     implementer = serializers.CharField(source='implementer.username')
     comments = serializers.SerializerMethodField()
-    task_name = serializers.CharField(read_only=False)
+
     class Meta:
         model = Tasks
         fields = ['task_proj', 'implementer', 'task_name', 'task_end', 'comments']
@@ -26,6 +26,16 @@ class TaskSerializer(serializers.ModelSerializer):
     def get_comments(self, obj):
         comments = Comments.objects.filter(comment_task=obj)
         return CommentSerializer(comments, many=True).data
+    
+    def update(self, instance, validated_data):
+        instance.task_proj.proj_name = validated_data.get('task_proj', instance.task_proj.proj_name)
+        instance.implementer.username = validated_data.get('implementer', instance.implementer.username)
+        instance.task_name = validated_data.get('task_name', instance.task_name)
+        instance.task_end = validated_data.get('task_end', instance.task_end)
+        instance.status = validated_data.get('status', instance.status)
+        instance.priority = validated_data.get('priority', instance.priority)
+        instance.save()
+        return instance
 
 class CommentSerializer(serializers.ModelSerializer):
     comment_name = serializers.CharField(source='comment_name.username')
