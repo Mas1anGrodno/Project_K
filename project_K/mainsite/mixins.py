07639b0c66a491d1cac757cc3d4a321e91336056
Kryhin Maksim <masian.grodno@gmail.com>
2024-08-20 17:45:58 +0300

    12131

--- project_K/mainsite/mixins.py
+++ project_K/mainsite/mixins.py
@@ -6,33 +6,13 @@ class LanguageMixin:
         return request.query_params.get('lang', 'en')
 
 
-    # def translate_content(self, content, lang):
-    #     from django.utils import translation
-    #     translation.activate(lang)
-    #     translated_content = {
-    #         key: _(value) if isinstance(value, str) else value
-    #         for key, value in content.items()
-    #     }
-    #     translation.deactivate()
-    #     return translated_content
-    
     def translate_content(self, content, lang):
         from django.utils import translation
         translation.activate(lang)
-    
-        def translate_item(item):
-            if isinstance(item, dict):
-                return {key: translate_item(value) for key, value in item.items()}
-            elif isinstance(item, list):
-                return [translate_item(element) for element in item]
-            elif isinstance(item, str):
-                return _(item)
-            else:
-                return item
-        translated_content = translate_item(content)
+        translated_content = {key: _(value) if isinstance(value, str) else value for key, value in content.items()}
         translation.deactivate()
         return translated_content
-
+    
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
         lang = self.get_language(request)
