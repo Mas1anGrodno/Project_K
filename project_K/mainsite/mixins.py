bcf687872deda6439cb577dac595ccd083098bfa
Kryhin Maksim <masian.grodno@gmail.com>
2024-08-20 17:17:03 +0300

    mixin with add cycle

--- project_K/mainsite/mixins.py
+++ project_K/mainsite/mixins.py
@@ -5,24 +5,34 @@ class LanguageMixin:
     def get_language(self, request):
         return request.query_params.get('lang', 'en')
 
+
     # def translate_content(self, content, lang):
-    #     # Активируем выбранный язык
     #     from django.utils import translation
     #     translation.activate(lang)
-    #     translated_content = {key: _(value) for key, value in content.items()}
+    #     translated_content = {
+    #         key: _(value) if isinstance(value, str) else value
+    #         for key, value in content.items()
+    #     }
     #     translation.deactivate()
     #     return translated_content
-
+    
     def translate_content(self, content, lang):
         from django.utils import translation
         translation.activate(lang)
-        translated_content = {
-            key: _(value) if isinstance(value, str) else value
-            for key, value in content.items()
-        }
+    
+        def translate_item(item):
+            if isinstance(item, dict):
+                return {key: translate_item(value) for key, value in item.items()}
+            elif isinstance(item, list):
+                return [translate_item(element) for element in item]
+            elif isinstance(item, str):
+                return _(item)
+            else:
+                return item
+        translated_content = translate_item(content)
         translation.deactivate()
         return translated_content
-    
+
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
         lang = self.get_language(request)
